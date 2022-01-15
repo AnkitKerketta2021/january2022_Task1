4d3adf98468479a44553d7cbb800ba9a157bf24e
AnkitKerketta2021 <ankitkerketta999.com>
2022-01-15 15:57:29 +0530

    added missing files in the public folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import MainApp from "./task1/MainApp";
 import "bootstrap/dist/css/bootstrap.min.css";
-import EnhancedTable from "./task1/components/DataTable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <EnhancedTable /> */}
         <MainApp />
       </header>
     </div>
